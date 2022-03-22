7c2e6cd05009c0b9371780d630962abf2a2e9816
I-JSPR-I <jaspermeersschaut@hotmail.com>
2022-03-22 09:03:19 +0100

    Add project files.

---
+++ 22-03-22 properties ex 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _22_03_22_properties_ex_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 22-03-22 properties ex 1/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _22_03_22_properties_ex_1
+{
+    class Student
+    {
+        public string fname;
+        public string lname;
+        public string klas;
+
+public string Fname
+        {
+            get { return; }
+            set { fname = value; }
+        }
+        
+    }
+}
